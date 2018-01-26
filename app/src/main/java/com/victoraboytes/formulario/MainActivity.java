f07108dd5eff7be4ea50d9511766478eda513d18
victoraboytes <victoraboytes@outlook.com>
2018-01-26 10:36:51 -0600

    Added String test = "Test"

--- app/src/main/java/com/victoraboytes/formulario/MainActivity.java
+++ app/src/main/java/com/victoraboytes/formulario/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String name, date, phone, email, contactDescription;
     private Button btnNext;
 
+    String test = "Test";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
